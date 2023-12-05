d27ad0af53609ba2cfe265659f453f9534f12eab
Erick Kramer <erickkramer@gmail.com>
2023-12-05 07:58:01 +0100

    [WIP]: continue with the approach

--- day1/main.go
+++ day1/main.go
@@ -6,10 +6,33 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
+	"unicode"
 )
 
 func CalibrateSequence(msg string) int {
-	// re := regexp.MustCompile(`\d`)
+	var digits []int
+	word_digits := []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	var partial_string_builder strings.Builder
+	for _, c := range msg {
+		// Check if char is digit
+		if unicode.IsDigit(c) {
+			digit := int(c - '0')
+			digits = append(digits, digit)
+			partial_string_builder.Reset()
+			continue
+		}
+		for iword_digit, word_digit := range word_digits {
+			if partial_digit == word_digit {
+				digits = append(digits, iword_digit)
+				partial_digit = ""
+				continue
+			}
+			if strings.HasPrefix(word_digit, partial_digit) {
+				continue
+			}
+		}
+	}
 	re := regexp.MustCompile(`(\d+|one|two|three|four|five|six|seven|eight|nine)?`)
 	matches := re.FindAllString(msg, -1)
 	result := 0

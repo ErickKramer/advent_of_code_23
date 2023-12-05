d4669d54f0e3917caa963c93238bd480c66ecf83
Erick Kramer <erickkramer@gmail.com>
2023-12-05 23:31:30 +0100

    [python]: Solve day 2 part 2

--- python/day2/cube_conundrum.py
+++ python/day2/cube_conundrum.py
@@ -8,14 +8,31 @@
 from pathlib import Path
 
 
-def is_game_impossible_1(
+def is_game_possible(
     game: list[dict[str, int]], max_red: int = 12, max_green: int = 13, max_blue: int = 14
 ) -> bool:
     for draw in game:
-        if draw.get("red", 0) > max_red or draw.get("green", 0) > max_green or draw.get("blue",0) > max_blue:
-            return True
+        if (
+            draw.get("red", 0) > max_red
+            or draw.get("green", 0) > max_green
+            or draw.get("blue", 0) > max_blue
+        ):
+            return False
 
-    return False
+    return True
+
+
+def find_power_game(game: list[dict[str, int]]) -> int:
+    max_cubes = {}
+    for draw in game:
+        for color, num_cubes in draw.items():
+            if num_cubes > max_cubes.get(color, 0):
+                max_cubes[color] = num_cubes
+
+    power = 1
+    for cubes in max_cubes.values():
+        power *= cubes
+    return power
 
 
 if __name__ == "__main__":
@@ -34,8 +51,16 @@ if __name__ == "__main__":
                 draws.append(parsed_draw)
             games.append(draws)
 
+    # Part 1
     sum = 0
     for i, game in enumerate(games):
-        if not is_game_impossible_1(game=game, max_red=12, max_green=13, max_blue=14):
+        if is_game_possible(game=game, max_red=12, max_green=13, max_blue=14):
             sum += i + 1
     print(f"Part 1: {sum}")
+
+    # Part 2
+    sum = 0
+    for i, game in enumerate(games):
+        power = find_power_game(game)
+        sum += power
+    print(f"Part 2: {sum}")
--- python/day2/test_cubes_conundrum.py
+++ python/day2/test_cubes_conundrum.py
@@ -7,8 +7,8 @@
 # Copyright 2023 Rethink Robotics, GbmH. All Rights Reserved.
 # ****************************************************************************
 import pytest
+from cube_conundrum import is_game_possible, find_power_game
 
-from cube_conundrum import is_game_impossible_1
 
 def test_is_game_possible():
     games = [
@@ -26,13 +26,41 @@ def test_is_game_possible():
         ],
         [{"red": 6, "blue": 1, "green": 3}, {"blue": 2, "red": 1, "green": 2}],
     ]
+    impossible_games = [True, True, False, False, True]
 
     sum = 0
     for i, game in enumerate(games):
-        if not is_game_impossible_1(game=game, max_red=12, max_green=13, max_blue=14):
+        game_status = is_game_possible(game=game, max_red=12, max_green=13, max_blue=14)
+        assert game_status == impossible_games[i]
+
+        if game_status:
             sum += i + 1
     assert sum == 8
 
+
+def test_find_game_power():
+    games = [
+        [{"blue": 3, "red": 4}, {"red": 1, "green": 2, "blue": 6}, {"green": 2}],
+        [{"blue": 1, "green": 2}, {"green": 3, "blue": 4, "red": 1}, {"green": 1, "blue": 1}],
+        [
+            {"green": 8, "blue": 6, "red": 20},
+            {"blue": 5, "red": 4, "green": 13},
+            {"green": 5, "red": 1},
+        ],
+        [
+            {"green": 1, "red": 3, "blue": 6},
+            {"green": 3, "red": 6},
+            {"green": 3, "blue": 15, "red": 14},
+        ],
+        [{"red": 6, "blue": 1, "green": 3}, {"blue": 2, "red": 1, "green": 2}],
+    ]
+    expected_powers = [48, 12, 1560, 630, 36]
+    sum = 0
+    for i, game in enumerate(games):
+        power = find_power_game(game)
+        assert power == expected_powers[i]
+        sum += power
+    assert sum == 2286
 
 
 if __name__ == "__main__":

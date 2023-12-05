8651a76ec7ab159e27bb0a5851551d77c98a26b1
Erick Kramer <erickkramer@gmail.com>
2023-12-05 07:32:29 +0100

    Rewrite code to work for both part 1 and part 2

--- day1/main.go
+++ day1/main.go
@@ -4,29 +4,59 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"regexp"
-	"strconv"
+	"unicode"
 )
 
-func CalibrateSequence(msg string) int {
-	re := regexp.MustCompile(`\d`)
-	matches := re.FindAllString(msg, -1)
+func CalibrateSequenceDigits(msg string) int {
+	var digits []int
+	for _, c := range msg {
+		if unicode.IsDigit(c) {
+			digit := int(c - '0')
+			digits = append(digits, digit)
+		}
+	}
 	result := 0
-	if len(matches) == 0 {
+	if len(digits) == 0 {
 		return result
 	}
-	if len(matches) > 1 {
-		first, _ := strconv.Atoi(matches[0])
-		last, _ := strconv.Atoi(matches[len(matches)-1])
-		result = first*10 + last
+	if len(digits) > 1 {
+		result = digits[0]*10 + digits[len(digits)-1]
 	} else {
-		first, _ := strconv.Atoi(matches[0])
-		result = first*10 + first
+		result = digits[0]*10 + digits[0]
+	}
+	return result
+}
+
+func CalibrateSequenceWords(msg string) int {
+	var digits []int
+	word_digits := []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	for ic, c := range msg {
+		// Check if char is digit
+		if unicode.IsDigit(c) {
+			digit := int(c - '0')
+			digits = append(digits, digit)
+		}
+		for iword_digit, word_digit := range word_digits {
+			word_length := len(word_digit)
+			if word_length <= len(msg[ic:]) && word_digit == msg[ic:ic+word_length] {
+				digits = append(digits, iword_digit)
+			}
+		}
+	}
+	result := 0
+	if len(digits) == 0 {
+		return result
+	}
+	if len(digits) > 1 {
+		result = digits[0]*10 + digits[len(digits)-1]
+	} else {
+		result = digits[0]*10 + digits[0]
 	}
 	return result
 }
 
 func main() {
+	// Read file
 	file, err := os.Open("./input.txt")
 	if err != nil {
 		fmt.Println("Failed to open file: ", err)
@@ -37,12 +67,23 @@ func main() {
 
 	// Create a scanner to read the file line by line
 	scanner := bufio.NewScanner(file)
-
-	// Iterate over each line
-	sum := 0
+	var codes []string
 	for scanner.Scan() {
 		line := scanner.Text()
-		sum += CalibrateSequence(line)
+		codes = append(codes, line)
 	}
-	fmt.Println(sum)
+
+	// Part 1
+	sum := 0
+	for _, code := range codes {
+		sum += CalibrateSequenceDigits(code)
+	}
+	fmt.Printf("Part1: %d\n", sum)
+
+	// Part 2
+	sum = 0
+	for _, code := range codes {
+		sum += CalibrateSequenceWords(code)
+	}
+	fmt.Printf("Part2: %d\n", sum)
 }
